19a47250c86e16278eb9cf0ea59ae3f04a62f2cf
angudadevops <anurag3242@gmail.com>
2019-12-27 10:16:08 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ for host in `cat inventory | egrep -v 'master|node'`
 	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S mkdir /root/.ssh"
 	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S touch /root/.ssh/authorized_keys"
 	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S apt install git ansible vim sshpass openssh-server -y"
-	cat /root/.ssh/id_rsa.pub | sshpass -p $1 ssh root@$host "cat >> /root/.ssh/authorized_keys"
+	cat /root/.ssh/id_rsa.pub | sshpass -p k8s123 ssh root@$host "cat >> /root/.ssh/authorized_keys"
    done
 
 ansible-playbook pre_requisites.yaml -i inventory
