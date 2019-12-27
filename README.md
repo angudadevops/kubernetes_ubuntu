154f3e6ea418d160beb549a108bd83c8df14ad93
Anurag Guda <anurag3242@gmail.com>
2019-12-27 10:12:05 -0800

    added install.sh

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 This repository will help you to install Kubernetes cluster with ansilbe on Ubuntu
 Prerequisites: 
-1. Spin up two nodes on vmware/virtualbox with ubuntu, here one node is master and another one node
-2. Please make sure to install ansible on master and copy the ssh keys to node, for more information go through the link https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-ansible-on-ubuntu-18-04
-3. Update you ansilbe hosts configuration with master and nodes under web group
-
-Once you're done with above steps run first pre_requisites.yaml and then 
-k8scluster.yaml. it will create a cluster with one master and one node. if you want to join more nodes you can add it in ansible hosts and update k8cluster.yaml based on that 
+1. Spin up two nodes on vmware/virtualbox with ubuntu, here one node is master and another one for node
+2. Please update your master and node ip's in inventory file
+3. make sure to use same login password for master and node
+4. Run the install.sh with input as your nodes password 
+example: 
+    ./install.sh k8s123
 
 Please raise an issue if you have any difficulties to install the kubernetes cluster with ansible 
 
---
+++ install.sh
@@ -0,0 +1,21 @@
+
+ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+
+sudo apt install git ansible vim sshpass openssh-server curl -y
+
+for host in `cat inventory | egrep -v 'master|node'`
+   do
+	echo $host
+	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S sed -i  's|PermitRootLogin prohibit-password|PermitRootLogin yes|' /etc/ssh/sshd_config"
+	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S sed -i  's|#PasswordAuthentication yes|PasswordAuthentication yes|' /etc/ssh/sshd_config"
+	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S cat /etc/ssh/sshd_config | grep RootLogin"
+	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S echo 'root:k8s123' | chpasswd"
+	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S systemctl restart sshd"
+	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S mkdir /root/.ssh"
+	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S touch /root/.ssh/authorized_keys"
+	sshpass -p $1 ssh anguda@$host "echo $1 | sudo -S apt install git ansible vim sshpass openssh-server -y"
+	cat /root/.ssh/id_rsa.pub | sshpass -p $1 ssh root@$host "cat >> /root/.ssh/authorized_keys"
+   done
+
+ansible-playbook pre_requisites.yaml -i inventory
+ansible-playbook k8scluster.yaml -i inventory
