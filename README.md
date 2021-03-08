826c859c188550d9255668890c664d5c6f5c7c6e
Anurag Guda <anuragmsu@gmail.com>
2021-03-08 21:47:00 +0000

    Kubernetes setup with multiple runtimes

--- README.md
+++ README.md
@@ -3,12 +3,22 @@
 - Prerequisites: 
 1. Spin up nodes on vmware/virtualbox with ubuntu/RHEL/CentOS, here one node is master and another one for node
 2. you can add multiple nodes to the masters 
-3. Please update your master and node ip's in inventory file
-4. make sure to use same login password for master and node
-5. Run the install.sh with input as your nodes password 
+3. Please update your master and node ip's in hosts file like below
+```
+[master]
+10.110.16.178 ansible_ssh_user=admin ansible_ssh_pass=pass ansible_sudo_pass=pass ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+[node]
+10.110.16.179 ansible_ssh_user=admin ansible_ssh_pass=pass ansible_sudo_pass=pass ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+```
+4. Update the Required runtime for kubernetes in runtime.yaml, Available runtimes are 
+
+- CRI-O
+- Containerd
+- Docker
+
 example: 
 ```
-    ./install.sh k8s123
+bash setup.sh install
 ```
 ### Please raise an issue if you have any difficulties to install the kubernetes cluster with ansible 
 
---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo -e "Usage: \n bash setup.sh [OPTIONS]\n \n Available Options: \n      install     Install Kubernetes Stack\n      uninstall   Uninstall Kubernetes Stack"
+	echo
+	exit 1
+fi
+
+sudo ls > /dev/null
+
+# Ansible Install
+ansible_install() {
+	os=$(cat /etc/os-release | grep -iw ID | awk -F'=' '{print $2}') 
+	version=$(cat /etc/os-release | grep -i VERSION_CODENAME | awk -F'=' '{print $2}')
+	if [[ $os == "ubuntu" && $version != "focal" ]]; then 
+		echo "Installing Ansible"
+        	sudo apt-add-repository ppa:ansible/ansible -y
+        	sudo apt update
+        	sudo apt install ansible sshpass -y
+	elif [[ $os == "ubuntu" && $version == "focal" ]]; then
+		echo "Installing Ansible"
+		sudo apt update
+        	sudo apt install ansible sshpass -y
+	elif [ $os == "rhel*" ]; then
+		version=$(cat /etc/os-release | grep VERSION_ID | awk -F'=' '{print $2}')
+		if [ $version == "*7.*" ]; then
+			sudo subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
+			sudo yum install ansible -y
+		elif [ $version == "*8.*" ]; then
+			sudo subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
+			sudo yum install ansible -y
+		fi
+	fi
+}
+if ! hash sudo ansible 2>/dev/null
+then 
+	ansible_install
+else
+	echo "Ansible Already Installed"
+	echo
+fi
+if [ $1 == "install" ]; then
+	echo
+	echo Container RunTime $(cat runtime.yaml | awk -F':' '{print $2}')
+	echo
+	echo "Installing Kubernetes Cluster"
+	cd ansible
+	ansible-playbook -i hosts prerequisites.yaml	
+	ansible-playbook -i hosts k8sinstall.yaml
+	cd ../
+elif [ $1 == "uninstall" ]; then
+	echo
+	echo "Unstalling Kubernetes Cluster"
+	cd ansible
+        ansible-playbook -i hosts k8suninstall.yaml
+	cd ../
+else
+	echo -e "Usage: \n bash setup.sh [OPTIONS]\n \n Available Options: \n      install     Install Kubernetes Stack\n      uninstall   Uninstall Kubernetes Stack"
+        echo
+        exit 1
+fi
+
